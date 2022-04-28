e52f6a47151a8210886964c911345c2cd0b6a6e1
xkcm <xkcm16@gmail.com>
2022-04-28 14:24:53 +0200

    Add credentials check

--- helpers.js
+++ helpers.js
@@ -1,4 +1,7 @@
 function readCredentials(parameters) {
+  if (!parameters.AWS_ACCESS_KEY_ID || !parameters.AWS_SECRET_ACCESS_KEY || !parameters.REGION) {
+    throw new Error("Access Key ID, Secret Access Key and Region are required parameters. Please specify them in the action's parameters or plugin's settings.");
+  }
   return {
     AWS_ACCESS_KEY_ID: parameters.AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY: parameters.AWS_SECRET_ACCESS_KEY,
