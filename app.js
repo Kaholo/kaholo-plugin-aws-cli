926739a1bce34726517ea8e8fddc2c2cb2a9afe0
xkcm <xkcm16@gmail.com>
2022-06-26 23:24:42 +0200

    Remove Test CLI method

--- app.js
+++ app.js
@@ -3,15 +3,6 @@ const kaholo = require("kaholo-plugin-library");
 const helpers = require("./helpers");
 const awsCli = require("./awscli");
 
-const TEST_COMMAND = "sts get-caller-identity";
-
-async function testCli(parameters) {
-  return runCommand({
-    ...parameters,
-    command: TEST_COMMAND,
-  });
-}
-
 async function runCommand(parameters) {
   const credentials = helpers.readCredentials(parameters);
   try {
@@ -23,7 +14,6 @@ async function runCommand(parameters) {
 }
 
 module.exports = kaholo.bootstrap({
-  testCli,
   runCommand,
 }, {
   listRegions: awsPluginLibrary.autocomplete.listRegions,
