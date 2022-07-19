552317d1e778cc1933299f4aef5ba7731aedcc1b
xkcm <xkcm16@gmail.com>
2022-07-19 10:54:51 +0200

    Refactor credentials names to use camel case

--- helpers.js
+++ helpers.js
@@ -2,14 +2,14 @@ const { access } = require("fs/promises");
 const { extname: getFileExtension } = require("path");
 
 function readCredentials(parameters) {
-  if (!parameters.AWS_ACCESS_KEY_ID || !parameters.AWS_SECRET_ACCESS_KEY || !parameters.REGION) {
+  if (!parameters.awsAccessKeyId || !parameters.awsSecretAccesssKey || !parameters.awsRegion) {
     throw new Error("Access Key ID, Secret Access Key and Region are required parameters. Please specify them in the action's parameters or plugin's settings.");
   }
 
   return {
-    AWS_ACCESS_KEY_ID: parameters.AWS_ACCESS_KEY_ID,
-    AWS_SECRET_ACCESS_KEY: parameters.AWS_SECRET_ACCESS_KEY,
-    AWS_DEFAULT_REGION: parameters.REGION,
+    AWS_ACCESS_KEY_ID: parameters.awsAccessKeyId,
+    AWS_SECRET_ACCESS_KEY: parameters.awsSecretAccessKey,
+    AWS_DEFAULT_REGION: parameters.awsRegion,
   };
 }
 
